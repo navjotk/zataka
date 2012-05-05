39457a5cd4e25b2e48444c34948d30ecacb1a5e1
Navjot Kukreja <navjot@gharpay.in>
2012-05-05 22:04:49 +0530

    implementing keyboard handler

--- player.js
+++ player.js
@@ -4,8 +4,13 @@ function Player(color, renderer, initial_position, initial_velocity) {
 	this.renderer.drawDot(initial_position.x, initial_position.y, this.color);
 	this.position = initial_position;
 	this.velocity = initial_velocity;
+	this.input = 0;
 }
 
 Player.prototype.moveBy = function(diff_x, diff_y) {
 	this.renderer.drawMove(this.position.x, this.position.y, diff_x, diff_y);
+}
+
+Player.prototype.setInput = function(input) {
+	this.input = input;
 }
